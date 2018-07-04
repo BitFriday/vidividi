9f1421ce7e528fd5b11cb6eeee54bb66787dd7a5
BitFriday <39190763+BitFriday@users.noreply.github.com>
2018-07-04 15:26:48 +0300

    vidi

--- src/xdnad.cpp
+++ src/xdnad.cpp
@@ -25,7 +25,7 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called VIRIDI (http://www.viridi.io),
+ * This is the developer documentation of the reference client for an experimental new digital currency called VIRIDI (http://www.xdna.io),
  * which enables instant payments to anyone, anywhere in the world. VIRIDI uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
@@ -65,7 +65,7 @@ bool AppInit(int argc, char* argv[])
     //
     // Parameters
     //
-    // If Qt is used, parameters/viridi.conf are parsed in qt/viridi.cpp's main()
+    // If Qt is used, parameters/xdna.conf are parsed in qt/xdna.cpp's main()
     ParseParameters(argc, argv);
 
     // Process help and version before taking care about datadir
@@ -112,11 +112,11 @@ bool AppInit(int argc, char* argv[])
         // Command-line RPC
         bool fCommandLine = false;
         for (int i = 1; i < argc; i++)
-            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "viridi:"))
+            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "xdna:"))
                 fCommandLine = true;
 
         if (fCommandLine) {
-            fprintf(stderr, "Error: There is no RPC client functionality in xdnad anymore. Use the viridi-cli utility instead.\n");
+            fprintf(stderr, "Error: There is no RPC client functionality in xdnad anymore. Use the xdna-cli utility instead.\n");
             exit(1);
         }
 #ifndef WIN32
