681fdc1f2af5757cf457ce45477561aef3a18c3e
BitFriday <39190763+BitFriday@users.noreply.github.com>
2018-07-06 13:02:11 +0300

    vidi

--- src/main.cpp
+++ src/main.cpp
@@ -1620,9 +1620,9 @@ double ConvertBitsToDouble(unsigned int nBits)
 CAmount GetBlockValue(int nHeight)
 {
     if (nHeight == 0) {
-        return 1 * COIN;
+        return 69444 * COIN;
     } else if (nHeight < Params().ANTI_INSTAMINE_TIME()) {
-        return 1 * COIN;
+        return 69444 * COIN;
 
       // POS Year 1
     } else if (nHeight <= 1965600 && nHeight > Params().LAST_POW_BLOCK()) {
